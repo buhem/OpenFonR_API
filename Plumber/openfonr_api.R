daf47028047e8cd0276a8cf6333528f86713dc67
EL IDRISSI Mohamed <34623031+buhem@users.noreply.github.com>
2020-08-15 17:34:23 +0200

    Update openfonr_api.R

--- Plumber/openfonr_api.R
+++ Plumber/openfonr_api.R
@@ -5,6 +5,27 @@
 
 
 
+
+#* @filter cors
+cors <- function(req, res) {
+  
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods","*")
+    res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200 
+    return(list())
+  } else {
+    plumber::forward()
+  }
+  
+}
+
+
+
+
+
 #* sound
 #* @param callType Choose the item
 #* @param speaker Choose the informant
